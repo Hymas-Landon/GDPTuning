870830210b18a274ca7d177e071934edd8b86515
Landon <landocomando_8@msn.com>
2018-11-07 14:43:05 -0700

    11-07-2018

--- app/src/main/java/com/gdptuning/gdptuning/LiveDataDigitalFragment.java
+++ app/src/main/java/com/gdptuning/gdptuning/LiveDataDigitalFragment.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -26,8 +25,6 @@ import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -41,42 +38,18 @@ public class LiveDataDigitalFragment extends Fragment {
     private static int VRAM = 11;
     //ESP32 aREST server address
     final String url = "http://192.168.7.1";
-    //        final String url = "https://api.myjson.com/bins/17x8hg";
-
-    List<Integer> lstImages = new ArrayList<>();
     boolean isConnected = false;
     boolean isProcessing = false;
     String device = "GDP";
     int tuneMode = 0;
-    String faren = "℉";
-    String cels = "\u2103";
     Timer timer;
-    TextView tvBoostView, tvEgt, tvOilPressure, tvFuel, tvTurbo, tvCoolant;
     RequestQueue queue;
     WifiManager wifi;
-
-//    //Gauges
-//    Gauge gauge1;
-//    Gauge gauge2;
-//    Gauge gauge3;
-//    Gauge gauge4;
-//    Gauge gauge5;
-//    Gauge gauge6;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View mView = inflater.inflate(R.layout.fragment_livedata_digital, container, false);
-
-
-        //Gauges information
-//        gauge1 = mView.findViewById(R.id.gauge1);
-//        gauge2 = mView.findViewById(R.id.gauge2);
-//        gauge3 = mView.findViewById(R.id.gauge3);
-//        gauge4 = mView.findViewById(R.id.gauge4);
-//        gauge5 = mView.findViewById(R.id.gauge5);
-//        gauge6 = mView.findViewById(R.id.gauge6);
-
         return mView;
     }
 
@@ -85,7 +58,7 @@ public class LiveDataDigitalFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         //Working with wifi
-        queue = Volley.newRequestQueue(getActivity());
+        queue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
         wifi = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         sendRequest();
         timer = new Timer();
@@ -186,44 +159,30 @@ public class LiveDataDigitalFragment extends Fragment {
                             float coolant = variables.getInt("coolant");
                             float oil_pressure = variables.getInt("oil_pressur");
 
+                            ImageIndicator mImageIndicatorSmall = new ImageIndicator(Objects.requireNonNull(getContext()), R.drawable.needle2);
+                            ImageIndicator mImageIndicatorLarge = new ImageIndicator(Objects.requireNonNull(getContext()), R.drawable.needle1);
+
 
 
 //                            if (getVehicleType() == VFORD1 || getVehicleType() == VFORD2) {
 //                                TextView oilText = getView().findViewById(R.id.title4);
 //                                oilText.setText("Oil \nTemp");
 //                                float fordOilTemp = variables.getInt("oil_temp");
-//                                //Gauge1
-//                                ImageSpeedometer imageSpeedometer1 = getView().findViewById(R.id.speedGauge1);
-//                                imageSpeedometer1.speedTo((float) ((egt * 1.8) + 32));
-//
-//                                //Gauge2
-//                                ImageSpeedometer imageSpeedometer2 = getView().findViewById(R.id.speedGauge2);
-//                                if (boost > 5) {
-//                                    imageSpeedometer2.speedTo((float) (boost * 0.1450377));
-//                                } else {
-//                                    imageSpeedometer2.speedTo(0);
-//                                }
-//
-                            //Gauge3
-                            ImageIndicator mImageIndicatorSmall = new ImageIndicator(Objects.requireNonNull(getContext()), R.drawable.needle2);
-                            ImageSpeedometer imageSpeedometer3 = Objects.requireNonNull(getView()).findViewById(R.id.speedGauge4);
-                            imageSpeedometer3.setIndicator(mImageIndicatorSmall);
-                            imageSpeedometer3.speedTo(turbo);
-//
-//                                //Gauge4
-//                                ImageSpeedometer imageSpeedometer4 = getView().findViewById(R.id.speedGauge4);
-//                                imageSpeedometer4.speedTo((float) ((fordOilTemp * 1.8) + 32));
-//
-                            //Gauge5
-                            ImageSpeedometer imageSpeedometer5 = getView().findViewById(R.id.speedGauge2);
-                            imageSpeedometer5.setIndicator(mImageIndicatorSmall);
-                            imageSpeedometer5.speedTo(fuel);
 
-                            //Gauge6
-                            ImageIndicator mImageIndicatorLarge = new ImageIndicator(Objects.requireNonNull(getContext()), R.drawable.needle2);
-                            ImageSpeedometer imageSpeedometer6 = getView().findViewById(R.id.speedGauge3);
-                            imageSpeedometer6.setIndicator(mImageIndicatorLarge);
-                            imageSpeedometer6.speedTo((float) (oil_pressure * 0.145));
+                            // Turbo
+                            ImageSpeedometer turboVanes = Objects.requireNonNull(getView()).findViewById(R.id.turbo_vanes);
+                            turboVanes.setIndicator(mImageIndicatorSmall);
+                            turboVanes.speedTo(turbo);
+
+                            // Fuel
+                            ImageSpeedometer injectionFuel = getView().findViewById(R.id.injection_fuel);
+                            injectionFuel.setIndicator(mImageIndicatorSmall);
+                            injectionFuel.speedTo(fuel);
+
+                            // Oil Pressure
+                            ImageSpeedometer oilTemp = getView().findViewById(R.id.oil_temp);
+                            oilTemp.setIndicator(mImageIndicatorLarge);
+                            oilTemp.speedTo((float) (oil_pressure * 0.145));
 
 
 //                            } else if (getVehicleType() == VGM1 || getVehicleType() == VGM2 || getVehicleType() == VRAM) { //Gauge1
@@ -283,7 +242,7 @@ public class LiveDataDigitalFragment extends Fragment {
     //Show Connection details
     void displayDevicecInfo() {
         if (isConnected) {
-            new SweetAlertDialog(getActivity(), SweetAlertDialog.SUCCESS_TYPE)
+            new SweetAlertDialog(Objects.requireNonNull(getActivity()), SweetAlertDialog.SUCCESS_TYPE)
                     .setTitleText("Connected")
                     .setContentText("You are connected to " + device)
                     .setConfirmText("ok")
--- app/src/main/java/com/gdptuning/gdptuning/LiveDataDigitalFragment2.java
+++ app/src/main/java/com/gdptuning/gdptuning/LiveDataDigitalFragment2.java
@@ -8,10 +8,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,6 +20,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.github.anastr.speedviewlib.ImageSpeedometer;
+import com.github.anastr.speedviewlib.components.Indicators.ImageIndicator;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,48 +30,38 @@ import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import de.nitri.gauge.Gauge;
-
-public class LiveDataDigitalFragment2 extends Fragment {
+public class LiveDataDigitalFragment2 extends Fragment implements View.OnTouchListener, GestureDetector.OnGestureListener {
 
     private static int VFORD1 = 7;
     private static int VFORD2 = 8;
     private static int VGM1 = 9;
     private static int VGM2 = 10;
     private static int VRAM = 11;
+
     //ESP32 aREST server address
     final String url = "http://192.168.7.1";
-    //        final String url = "https://api.myjson.com/bins/17x8hg";
     boolean isConnected = false;
     boolean isProcessing = false;
     String device = "GDP";
     int tuneMode = 0;
     Timer timer;
-    TextView tvBoostView, tvEgt, tvOilPressure, tvFuel, tvTurbo, tvCoolant;
     RequestQueue queue;
     WifiManager wifi;
-
-    //Gauges
-    Gauge gauge1;
-    Gauge gauge2;
-    Gauge gauge3;
-    Gauge gauge4;
-    Gauge gauge5;
-    Gauge gauge6;
+    ImageSpeedometer egtGauge, boostGauge, coolantTemp;
+    private GestureDetector mGestureDetector;
+    public static final String TAG = "GDP Tuning";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View mView = inflater.inflate(R.layout.fragment_livedata_digital_2, container, false);
-
-        //connect textViews
-//        tvEgt = mView.findViewById(R.id.egt);
-//        tvBoostView = mView.findViewById(R.id.boost);
-//        tvTurbo = mView.findViewById(R.id.turbo);
-//        tvOilPressure = mView.findViewById(R.id.oil_pressure);
-//        tvFuel = mView.findViewById(R.id.fuel_rate);
-//        tvCoolant = mView.findViewById(R.id.coolant);
-
+        egtGauge = mView.findViewById(R.id.egt_temp);
+        boostGauge = mView.findViewById(R.id.boost);
+        coolantTemp = mView.findViewById(R.id.coolant_temp);
+        egtGauge.setOnTouchListener(this);
+        boostGauge.setOnTouchListener(this);
+        coolantTemp.setOnTouchListener(this);
+        mGestureDetector = new GestureDetector(getActivity(), this);
 
         return mView;
     }
@@ -79,7 +72,7 @@ public class LiveDataDigitalFragment2 extends Fragment {
 
 
         //Working with wifi
-        queue = Volley.newRequestQueue(getActivity());
+        queue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
         wifi = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         sendRequest();
         timer = new Timer();
@@ -98,7 +91,7 @@ public class LiveDataDigitalFragment2 extends Fragment {
     }
 
     private int getVehicleType() {
-        SharedPreferences mSharedPreferences = this.getActivity().getSharedPreferences("ThemeColor", Context.MODE_PRIVATE);
+        SharedPreferences mSharedPreferences = Objects.requireNonNull(this.getActivity()).getSharedPreferences("ThemeColor", Context.MODE_PRIVATE);
         return mSharedPreferences.getInt("vehicle", VFORD1);
     }
 
@@ -165,16 +158,24 @@ public class LiveDataDigitalFragment2 extends Fragment {
                             deviceName += response.getString("id");
                             device = deviceName;
 
+                            ImageIndicator mImageIndicatorSmall = new ImageIndicator(Objects.requireNonNull(getContext()), R.drawable.needle2);
+                            ImageIndicator mImageIndicatorLarge = new ImageIndicator(Objects.requireNonNull(getContext()), R.drawable.needle1);
+
                             float frp = variables.getInt("frp");
                             float timing = variables.getInt(("timing"));
+                            float coolant = variables.getInt("coolant");
 
-//                            //Gauge1
-//                            ImageSpeedometer imageSpeedometer1 = Objects.requireNonNull(getView()).findViewById(R.id.speedGauge1);
-//                            imageSpeedometer1.speedTo((float) (frp * 145.0377));
-//
-//                            //Gauge2
-//                            ImageSpeedometer imageSpeedometer2 = Objects.requireNonNull(getView()).findViewById(R.id.speedGauge2);
-//                            imageSpeedometer2.speedTo(timing);
+                            // EGT Temp Gauge
+                            egtGauge.setIndicator(mImageIndicatorSmall);
+                            egtGauge.speedTo((float) (frp * 145.0377));
+
+                            // Boost Gauges
+                            boostGauge.setIndicator(mImageIndicatorSmall);
+                            boostGauge.speedTo(timing);
+
+                            // Coolant Temp
+                            coolantTemp.setIndicator(mImageIndicatorLarge);
+                            coolantTemp.speedTo((float) ((coolant * 1.8) + 32));
 
                         } catch (JSONException e1) {
                             e1.printStackTrace();
@@ -197,4 +198,87 @@ public class LiveDataDigitalFragment2 extends Fragment {
         // add it to the RequestQueue
         queue.add(getRequest);
     }
+
+
+    @Override
+    public boolean onTouch(View mView, MotionEvent mMotionEvent) {
+        mGestureDetector.onTouchEvent(mMotionEvent);
+
+        int action = mMotionEvent.getAction();
+
+        switch (action) {
+            case (MotionEvent.ACTION_DOWN):
+                Log.d(TAG, "Action was DOWN");
+                return true;
+            case (MotionEvent.ACTION_MOVE):
+                Log.d(TAG, "Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP):
+                Log.d(TAG, "Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL):
+                Log.d(TAG, "Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE):
+                Log.d(TAG, "Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default:
+                return mView.onTouchEvent(mMotionEvent);
+        }
+//
+//
+//        if (getView().getId() == R.id.egt_temp) {
+//            mGestureDetector.onTouchEvent(mMotionEvent);
+//            return true;
+//        }
+//        if (getView().getId() == R.id.coolant_temp) {
+//            return false;
+//        }
+//        if (getView().getId() == R.id.boost) ;
+//        {
+//
+//        }
+//
+//        return true;
+    }
+
+    /*
+     * Gesture Detectors
+     * */
+    @Override
+    public boolean onDown(MotionEvent mMotionEvent) {
+        Log.d(TAG, "onDown: called");
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent mMotionEvent) {
+        Log.d(TAG, "onShowPress: called");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent mMotionEvent) {
+        Log.d(TAG, "onSingleTapUp: called");
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent mMotionEvent, MotionEvent mMotionEvent1, float mV, float mV1) {
+        Log.d(TAG, "onScroll: called");
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent mMotionEvent) {
+        Log.d(TAG, "onLongPress: called");
+    }
+
+    @Override
+    public boolean onFling(MotionEvent mMotionEvent, MotionEvent mMotionEvent1, float mV, float mV1) {
+        Log.d(TAG, "onFling: called");
+        return false;
+    }
+
+
 }
